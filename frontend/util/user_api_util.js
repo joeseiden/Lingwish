3a9e5d9ae64428111d3c97fc04a54718a8ae02bc
joeseiden <joe.seiden214@gmail.com>
2017-05-12 15:36:23 -0700

    Create session util and routes

--- frontend/util/user_api_util.js
+++ frontend/util/user_api_util.js
@@ -2,4 +2,18 @@ export const fetchUser = userId => (
   $.ajax({
     url: `api/users/${userId}`
   })
-)
+);
+
+export const editAccount = user => (
+  $.ajax({
+    type: 'PATCH',
+    url: `api/users/${user.id}`
+  })
+);
+
+export const deleteAccount = user => (
+  $.ajax({
+    type: 'DELETE',
+    url: `api/users/${user.id}`
+  })
+);
