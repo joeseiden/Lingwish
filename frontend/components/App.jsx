055e442a344ca41c16e2d0e4b0d011e2615bf5d5
joeseiden <joe.seiden214@gmail.com>
2017-05-16 15:29:41 -0700

    Create route util, update app and route

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+import AuthFormContainer from './session_auth/auth_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 
 const App = (props) => (
   <div>
-    { props.children }
+    <header>
+      <Link to="/" className="header-link">
+        <h1>lɪŋ'ɡwɪʃ</h1>
+      </Link>
+    </header>
+    <Switch>
+      <AuthRoute path="/login" component={AuthFormContainer} />
+      <AuthRoute path="/signup" component={AuthFormContainer} />
+    </Switch>
   </div>
 );
 
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import {
-  Router,
-  Route,
-  IndexRoute,
-  hasHistory
-} from 'react-router';
+import { HashRouter } from 'react-router';
 import App from './App';
 
-const Root = ({ store }) => {
+const Root = ({ store }) => (
+  <Provider store={ store }>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+);
 
-  return (
-    <Provider store={ store }>
-      <Router history={ hasHistory }>
-        <Route path="/" component={ App }>
-
-        </Route>
-      </Router>
-    </Provider>
-  );
-};
 
 export default Root;
