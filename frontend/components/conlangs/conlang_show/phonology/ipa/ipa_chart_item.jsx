994481bc53b73d0ee91575cb8e89bcd15be7b996
Joe Seiden <joe.seiden214@gmail.com>
2017-07-06 00:49:39 -0700

    Create DRYer IpaChart instead of separate components for consonant/vowel charts

---
+++ frontend/components/conlangs/conlang_show/phonology/ipa/ipa_chart_item.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const IpaChartItem = props => {
+  const inventory = props.inventory.map(phoneme => phoneme.id);
+  const phonemes = props.phonemes;
+
+  return (
+    <td>
+      {phonemes.map(phoneme => {
+          let classes = classNames({
+            'ipa': true,
+            'selected': inventory.includes(phoneme.id)
+          });
+
+          return (
+            <span
+              key={phoneme.id}
+              value={phoneme.id}
+              className={classes}
+              title={phoneme.name}
+              onClick={props.selectPhoneme.bind(this, phoneme.id)}>
+              {phoneme.char}
+            </span>
+          );
+      })}
+    </td>
+  );
+};
+
+export default IpaChartItem;
