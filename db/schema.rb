0050a0a263a7420b076cc83831ae65bb91bf59a4
Joe Seiden <joe.seiden214@gmail.com>
2017-08-01 15:05:04 -0700

    Create syllables migration for phonology table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707201959) do
+ActiveRecord::Schema.define(version: 20170801215403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170707201959) do
     t.integer  "conlang_id",                       null: false
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
+    t.string   "syllables",                                     array: true
   end
 
   create_table "users", force: :cascade do |t|
--- frontend/components/user_profile/user_conlangs/user_conlangs_index_item.jsx
+++ frontend/components/user_profile/user_conlangs/user_conlangs_index_item.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import classNames from 'classNames';
+import classNames from 'classnames';
 
 class UserConlangsIndexItem extends React.Component {
   constructor(props) {
